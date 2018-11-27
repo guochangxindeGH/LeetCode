97e37cdd5f30ecc0e9c6d0fedb02695bb94242c1
Jon <291976422@qq.com>
2018-11-27 21:30:47 +0800

    add 88 solution

---
+++ Algorithms/88 - Merge Sorted Array/01 - Merge Sorted Array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1.length = m
+    nums2.length = n
+    Array.prototype.push.apply(nums1,nums2)
+    nums1.sort(function(a,b){
+        return a-b
+    })
+
+};
