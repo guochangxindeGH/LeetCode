ea5f3015ff20e7896878610cb06f5dc4cefc4867
Jon <291976422@qq.com>
2024-04-03 17:37:09 +0800

    #feat：19. 删除链表的倒数第 N 个结点

--- Algorithms/19 - Remove Nth Node From End of List/02 - Remove Nth Node From End of List.js
+++ Algorithms/19 - Remove Nth Node From End of List/02 - Remove Nth Node From End of List.js
@@ -1,4 +1,19 @@
 /**
+ * 19. 删除链表的倒数第 N 个结点
+ * 
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5], n = 2
+    输出：[1,2,3,5]
+
+    示例 2：
+
+    输入：head = [1], n = 1
+    输出：[]
+
+    示例 3：
+
+    输入：head = [1,2], n = 1
+    输出：[1]
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
@@ -11,23 +26,23 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    var length=0;
-    var it=head;
+    let length = 0;
+    let it = head;
     while(it){
-        length++;
-        it=it.next
+        length ++;
+        it = it.next
     }
-    n=length-n;
-    it=head
-    if(n==0){
+    n = length - n;
+    it = head
+    if(n === 0){
         return head.next;
     }
     else{
-        while(n>1){
-            it=it.next;
-            n--
+        while(n > 1){
+            it = it.next;
+            n --
         }
-        it.next=it.next.next
+        it.next = it.next.next
     }
     return head;
 };
